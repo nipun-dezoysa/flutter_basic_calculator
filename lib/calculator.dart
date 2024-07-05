16b7e36acab4dc26a8856d794783dfd9848132c7
nipun-dezoysa <nipunavishka123@gmail.com>
2024-07-05 23:57:30 +0530

    page linked to widget tree

---
+++ lib/calculator.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        home: Scaffold(body: Center(child: Text('Hello, World!'))));
+  }
+}
